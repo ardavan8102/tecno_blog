897f61ca653d409384e699fc2d0a48d695dbb6b9
ardavan8102 <eskandariardavan81@gmail.com>
2025-11-30 23:05:43 +0330

    GET Method created for api

---
+++ lib/services/dio_service.dart
@@ -0,0 +1,25 @@
+import 'dart:developer';
+import 'package:dio/dio.dart';
+
+class DioService {
+
+  Future<dynamic> getMethod(String url) async {
+
+    Dio dio = Dio();
+
+    dio.options.headers['content-type'] = 'application/json';
+
+    return await dio.get(
+      url,
+      options: Options(
+        responseType: .json,
+        method: 'GET',
+      ),
+    ).then((response){
+      log(response.toString());
+      return response;
+    });
+
+  }
+
+}
